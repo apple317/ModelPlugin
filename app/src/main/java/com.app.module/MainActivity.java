878b9453a859f52f350b2b8a0dd87cfb7c5370c9
applehsp <513888967@qq.com>
2020-04-01 19:41:43 +0800

    no message

--- app/src/main/java/com.app.module/MainActivity.java
+++ app/src/main/java/com.app.module/MainActivity.java
@@ -3,11 +3,10 @@ package com.app.module;
 import android.app.Activity;
 import android.os.Bundle;
 
-import androidx.annotation.Nullable;
 
 public class MainActivity extends Activity {
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
